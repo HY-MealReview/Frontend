b28b1a8fd56f5ae19aee944398455f84b3b0b492
Yean <kjan0929@naver.com>
2024-10-07 22:24:46 +0900

    chore: 레이아웃 세팅

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from "react";
+import styled from "styled-components";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <div>{children}</div>;
+  return <Wrapper>{children}</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  min-width: 360px;
+  max-width: 400px;
+  height: 100vh;
+  background-color: white;
+  padding-bottom: 54px;
+  margin: 0 auto;
+  overflow: auto;
+`;
