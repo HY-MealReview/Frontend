618498e5f9ed8733e7a7a844027ff0e696a1fc94
Yean <kjan0929@naver.com>
2024-10-07 22:20:11 +0900

    chore: 페이지별 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "@components/common/Layout";
 import { MainPage } from "@pages/main/main";
+import { LoginPage } from "@pages/login/login";
+import { WeeklyMenuPage } from "@pages/weekly-menu/weekly-menu";
+import { SettingPage } from "@pages/setting/setting";
 
 function App() {
   return (
@@ -9,6 +12,15 @@ function App() {
         <Routes>
           {/* main page */}
           <Route path="/" element={<MainPage />} />
+
+          {/* login page */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* weekly menu page */}
+          <Route path="/weekly-menu" element={<WeeklyMenuPage />} />
+
+          {/* setting page */}
+          <Route path="/setting" element={<SettingPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
