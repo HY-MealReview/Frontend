c47d399d1e926b1919a90249d929d9c4a027bedf
minjee2758 <minjee2758@naver.com>
2024-10-10 18:01:06 +0900

    feat : 로고, 메뉴 데이터 작성

---
+++ src/pages/main/main-types.tsx
@@ -0,0 +1,47 @@
+import menuImage from "@assets/main/menuImage.webp";
+
+//메뉴 데이터
+export interface MenuItem {
+    name: string; //이름
+  }
+  
+  //식당 정보들 - 식당 이름, 메뉴 이미지, 메뉴들
+  export interface Store {
+    id: number;
+    name: string; //식당 이름
+    imageUrl: string; //메뉴 이미지
+    mainMenu: MenuItem[]; //메뉴 종류
+    time: string; //운영 시간
+    location: string; //식당 위치
+    date : string; //날짜
+  }
+
+export const stores : Store[] =[
+    {
+        id: 1,
+        name: "학생 식당",
+        time: "17:00~22:00",
+        location: "학생복지관 2층",
+        imageUrl: menuImage,
+        mainMenu: [
+          { name: "전주식 콩나물 해장국" },
+          { name: "전주식 콩나물 해장국" },
+          { name: "전주식 콩나물 해장국" }
+        ],
+        date : "2100년 10월 349일"
+      },
+      {
+        id: 2,
+        name: "학생 식당",
+        time: "17:00~22:00",
+        location: "학생복지관 2층",
+        imageUrl: menuImage,
+        mainMenu: [
+          { name: "전주식 콩나물 해장국" },
+          { name: "전주식 콩나물 해장국" },
+          { name: "전주식 콩나물 해장국" }
+        ],
+        date : "2100년 10월 350일"
+      }
+
+];
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,3 +1,35 @@
+//import { useState } from 'react';
+import LogoImage from "@assets/main/logo.webp";
+import { stores } from "./main-types";
+
+
 export const MainPage = () => {
-  return <p>메인 페이지입니다</p>;
+
+  return (
+    <div>
+      <div style={{display:'flex', top:'0',  justifyContent:'center', marginBottom:'12px', marginTop:'12px', position: 'sticky'}}>
+        <img src={LogoImage} style={{ width: '85px', height: 'auto' }}/>
+      </div>
+
+      <div className='menuSliderContainer' style={{overflow:'hidden', width:'1000px' }}>
+        <div className="menuSlider" style={{}}>
+        {stores.map((store, index) => (
+          
+            <div className="menu1" key={store.id} onClick={() => handleMenuChange(index)} style={{ float: 'left', width: '328px', height: '482px', marginRight:'8px', marginBottom:'10px', boxShadow: '10px 4px 10px rgba(0, 0, 0, 0.1)', borderRadius: '12px'  }}>
+              <img src={store.imageUrl} style={{ width: '328px', height: 'auto' }} alt={store.name} />
+              <div className="menuName" style={{ margin:'14px', }}>
+                {store.mainMenu.map((menuItem, index) => (
+                  <div key={index} >{menuItem.name}</div>
+                ))}
+              </div>
+
+              </div>
+
+
+          ))}
+        </div>
+      </div>
+
+    </div>
+  );
 };
