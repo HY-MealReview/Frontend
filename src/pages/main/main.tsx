4c19ff857f50778dcb5774d4358fceb23bfbd430
minjee2758 <minjee2758@naver.com>
2024-10-20 10:35:46 +0900

    feat : 버튼 강조, 슬라이드 미리보기

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import LogoImage from "@assets/main/logo.webp";
+import Recommend from "@assets/main/Recommend.webp";
 import Slider from "react-slick";
 import { stores, Store } from '@pages/main/main-types';
 import 'slick-carousel/slick/slick.css';
@@ -10,10 +11,10 @@ export const MainPage: React.FC = () => {
   const [currentMeal, setCurrentMeal] = useState<string>('아침');
   const [currentStoreIndex, setCurrentStoreIndex] = useState<number>(0);
   const [currentStore, setCurrentStore] = useState<Store | null>(null);
-  const [isRecommended, setIsRecommended] = useState(false);
+  const [isRecommended, setIsRecommended] = useState(false);//추천
+  const [isNotRecommended, setIsNotRecommended] = useState(false);//비추천
   const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0); 
-  const [selectedStoreIndex, setSelectedStoreIndex] = useState<number | null>(null); 
-  
+  const [selectedStoreIndex, setSelectedStoreIndex] = useState<number>(0);  
 
   const settings = {
     //dots: true,
@@ -21,6 +22,8 @@ export const MainPage: React.FC = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    centerMode: true, 
+    centerPadding: '30px', //옆에 메뉴 살짝 미리보기 할 수 있게
     swipeToSlide: true, 
     touchThreshold: 10,
     draggable: true, 
@@ -46,7 +49,21 @@ export const MainPage: React.FC = () => {
 
 
   const handleRecommendClick = () => {
-    setIsRecommended(!isRecommended); 
+    if (isRecommended) {
+      setIsRecommended(false); // 다시 누르면 해제하기
+    } else {
+      setIsRecommended(true);  // 추천 버튼 활성화
+      setIsNotRecommended(false); // 비추천 버튼 비활성화
+    }
+};
+
+const handleNotRecommendClick = () => {
+  if (isNotRecommended) {
+    setIsNotRecommended(false); // 다시 누르면 해제하기
+  } else {
+    setIsRecommended(false); // 추천 버튼 비활성화
+    setIsNotRecommended(true);// 비추천 버튼활성화
+  }
 };
 
 
@@ -71,13 +88,13 @@ export const MainPage: React.FC = () => {
       <div className="slider-container" style={{width : '100%', height:'500px', margin:'0 auto', alignItems:'left'}}>
         <Slider {...settings}>
           {stores.map((food) => (
-            <div key={food.id} className="slide" style ={{ display: 'flex', flexDirection: 'column', marginLeft:'5px'}}>
+            <div key={food.id} className="slide" style ={{ display: 'flex', flexDirection: 'column', margin:'0 10px'}}>
               <div style={{width: '328px', height: '482px', margin: '0 auto', marginBottom:'20px',boxShadow : '0 0px 20px rgba(0,0,0,0.1)', borderRadius:'12px'}}>
                 <img src={food.imageUrl} className="menu-image" style={{width:'328px', height:'auto',alignItems: 'center'}} />
                 <div style={{margin : '12px'}}>
                   <ul style={{width : '304px', height : '72px', marginBottom :'8px'}}>
                     {food.mainMenu.map((item, index) => (
-                      <li key={index}>{item.name}</li>
+                      <li key={index} style={{textAlign: 'left'}}>• {item.name}</li>
                     ))}
                   </ul>
                   <div className="buttons" style={{display: 'flex', alignItems:'center', justifyContent:'center', gap:'24px'}}>
@@ -88,19 +105,28 @@ export const MainPage: React.FC = () => {
                       style={{width:'124px', height:'104px', borderRadius:'10px', cursor: 'pointer',
                         fontSize : '14px', fontWeight : isRecommended ? 'bold' : 'normal',
                         color : isRecommended ? '#134B84' : '#6A6A6A' ,
-                              border: isRecommended ? '2px solid #134B84' : '1px solid #F0F0F0',
+                        border: isRecommended ? '2px solid #134B84' : '1px solid #F0F0F0',
+                        display : 'flex',
+                        alignItems : 'center',
+                        justifyContent : 'center',
+                        gap : '9px'
                             }}
                       onClick={handleRecommendClick}>
+                        <img src={Recommend} style={{width : '48px', height : 'auto', padding : '6px'}}/>
                         추천
                       </div>
                     </div>
                     <div>
+                      <div style={{fontSize : '12px', color : '#444444', marginBottom : '12px'}}>비추천</div>
                       <div 
                       style={{width:'124px', height:'104px', borderRadius:'10px', cursor: 'pointer',
-                        fontSize : '14px', fontWeight : isRecommended ? 'bold' : 'normal',
-                        color : isRecommended ? '#134B84' : '#6A6A6A' ,
-                              border: isRecommended ? '2px solid #134B84' : '1px solid #F0F0F0',}}
-                      onClick={handleRecommendClick}>
+                        fontSize : '14px', fontWeight : isNotRecommended ? 'bold' : 'normal',
+                        color : isNotRecommended ? '#134B84' : '#6A6A6A' ,
+                              border: isNotRecommended ? '2px solid #134B84' : '1px solid #F0F0F0',
+                              display : 'flex',
+                              alignItems : 'center',
+                              justifyContent : 'center',}}
+                      onClick={handleNotRecommendClick}>
                         비추천</div>
                     </div>
                   </div>
