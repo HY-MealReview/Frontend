d8791e40ac469ba8a6dbda56d522bfbe354c6ed2
Yean <kjan0929@naver.com>
2024-11-08 23:16:50 +0900

    fix: 빌드 오류 수정

--- src/pages/weekly-menu/menuCard.tsx
+++ src/pages/weekly-menu/menuCard.tsx
@@ -1,4 +1,4 @@
-import noImage from '../../assets/weekly/noimage.jpg'; // noimage 파일 경로 설정
+import noImage from "../../assets/weekly/noImage.jpg"; // noimage 파일 경로 설정
 
 interface MenuCardProps {
   imageSrc: string; // 이미지 경로
@@ -8,20 +8,20 @@ interface MenuCardProps {
 
 function MenuCard({ imageSrc, rating, menuItems }: MenuCardProps) {
   const hasMenuItems = menuItems && menuItems.length > 0;
-  const hasRating = typeof rating === 'number';
+  const hasRating = typeof rating === "number";
 
   return (
     <div className="h-[150px] flex bg-white border-[1px] border-solid border-[#F0F0F0] rounded-lg mb-[8px] items-center">
       <div className="relative w-[148px] h-full flex justify-center items-center">
-        <img 
-          src={hasMenuItems && hasRating ? imageSrc : noImage} 
-          alt="Menu" 
-          className="w-full h-full object-cover rounded-l-lg" 
+        <img
+          src={hasMenuItems && hasRating ? imageSrc : noImage}
+          alt="Menu"
+          className="w-full h-full object-cover rounded-l-lg"
         />
         {hasMenuItems && hasRating ? (
           <div className="absolute flex items-center text-[24px] font-bold gap-[12px] border-[1px] border-[#F0F0F0]">
-            <div className='text-[#F0F900] text-[50px] '>★ </div>
-            <div className='text-white'>{rating}</div>
+            <div className="text-[#F0F900] text-[50px] ">★ </div>
+            <div className="text-white">{rating}</div>
           </div>
         ) : null}
       </div>
@@ -34,7 +34,7 @@ function MenuCard({ imageSrc, rating, menuItems }: MenuCardProps) {
             {menuItems.map((item, index) => (
               <div key={index} className="flex justify-between items-center ">
                 <span className="">{item.name}</span>
-                <div className='flex justify-center items-center gap-[4px]'>
+                <div className="flex justify-center items-center gap-[4px]">
                   <span className="text-[#F0F900] text-[30px] ">★</span>
                   <span className="ml-[2px]">{item.score}</span>
                 </div>
@@ -42,7 +42,9 @@ function MenuCard({ imageSrc, rating, menuItems }: MenuCardProps) {
             ))}
           </div>
         ) : (
-          <div className="p-[12px] text-[12px] text-center text-black">오늘의 메뉴가 없습니다.</div>
+          <div className="p-[12px] text-[12px] text-center text-black">
+            오늘의 메뉴가 없습니다.
+          </div>
         )}
       </div>
     </div>
